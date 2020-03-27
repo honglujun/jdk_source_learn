b4e1bb63839950266c49e7eefba1f2d59bbaff64
honglujun <909408954@qq.com>
2020-03-27 15:13:39 +0800

    jdk源码

---
+++ README.md
@@ -0,0 +1,8 @@
+# JDK源码包建议阅读顺序:lang>util>io>nio>math>time>net
+
+# 包内重要代码:
+
+## Lang:包内文件,annotation,reflect
+
+## Util:包内文件,concurrent,function,stream.
+
---
+++ src/test/java/util/HashMapTest.java
@@ -0,0 +1,14 @@
+package test.java.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("k1", "v1");
+        map.put("k2", "v2");
+        map.put("k3", "v3");
+        map.put("k4", "v4");
+    }
+}
